24b2e51e82f9200f822f4eebf5117bfc52a0aad0
ybrahni <yassinebrahni@gmail.com>
2024-05-26 17:59:59 +0100

    Refactor and enhance security headers plugin
    - Restructured plugin to streamline header management
    - Added default header values for 'X-Frame-Options', 'Strict-Transport-Security', 'Content-Security-Policy', 'X-Content-Type-Options', 'Referrer-Policy', and 'Permissions-Policy'
    - Implemented dynamic header retrieval from WordPress options with fallbacks to default values
    - Introduced custom and selectable 'Content-Security-Policy' options in settings
    - Added a settings page in the admin panel for configuring security headers
    - Created dropdowns for selecting predefined values for 'X-Frame-Options', 'Strict-Transport-Security', 'X-Content-Type-Options', 'Referrer-Policy', and 'Permissions-Policy'
    - Included JavaScript for checking website security headers via securityheaders.com
    - Added sanitization functions for custom input values
    - Set default options on plugin activation
    - Cleaned up options on plugin deactivation
    - Ensured proper function availability checks and direct access prevention

--- wordpress-header-security.php
+++ wordpress-header-security.php
@@ -5,52 +5,58 @@ Plugin URI: https://www.yassinebrahni.com/wordpress/contribution/my-security-hea
 Description: A plugin to configure multiple security headers
 Version: 1.0.0
 Author: Yassine Brahni
-Author URI: https://www.yassinebrahni.com/
-License: GPLv2 or later
+Author URI: https://www.linkedin.com/in/ybrahni/
 Text Domain: WPHeaderSecurity
+ * License: GPL v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
-if ( ! defined( 'ABSPATH' ) ) {
-    die( 'Invalid request.' );
+// Prevent direct access to the file
+if (!defined('ABSPATH')) {
+    die('You are not allowed to call this page directly.');
 }
 
-function wp_header_security() {
-    $headers = array(
-        'X-Frame-Options' => 'SAMEORIGIN',
-        'Strict-Transport-Security' => 'max-age=31536000; includeSubDomains',
-        'Content-Security-Policy' => "default-src 'self'; script-src 'self' 'unsafe-inline'; img-src 'self' data:;",
-        'X-Content-Type-Options' => 'nosniff',
-        'Referrer-Policy' => 'strict-origin-when-cross-origin',
-        'Permissions-Policy' => "geolocation 'none'; camera 'none'; microphone 'none';",
-    );
-
-    foreach ($headers as $header => $default_value) {
-        $value = get_option('wp_header_security_' . $header, $default_value);
-        if ($value) {
-            header($header . ': ' . $value);
-        }
-    }
-
-    // If custom Content-Security-Policy input is set, use it
-    $custom_csp = get_option('wp_header_security_Content-Security-Policy_Custom', '');
-    if ($custom_csp) {
-        header('Content-Security-Policy: ' . $custom_csp);
-    } else if ($value = get_option('wp_header_security_Content-Security-Policy_Select')) {
-        header('Content-Security-Policy: ' . $value);
-    }
+// Ensure WordPress functions are available
+if (!function_exists('add_action')) {
+    die('You are not allowed to call this page directly.');
 }
 
-add_action('send_headers', 'wp_header_security');
+const WPHS_PLUGIN_VERSION = '1.0.0';
+const WPHS_STANDARD_VALUE_CSP = "default-src 'self'; script-src 'self' 'unsafe-inline'; img-src 'self' data:;";
+const WPHS_STANDARD_VALUE_PERMISSIONS_POLICY = "geolocation 'none'; camera 'none'; microphone 'none';";
 
-function wp_header_security_settings_page() {
+function wphs_get_headers(array $headers = array()): array {
+    $headers['X-Frame-Options'] = get_option('wphs_X-Frame-Options', 'SAMEORIGIN');
+    $headers['Strict-Transport-Security'] = get_option('wphs_Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+    $headers['Content-Security-Policy'] = wphs_get_csp_header();
+    $headers['X-Content-Type-Options'] = get_option('wphs_X-Content-Type-Options', 'nosniff');
+    $headers['Referrer-Policy'] = get_option('wphs_Referrer-Policy', 'strict-origin-when-cross-origin');
+    $headers['Permissions-Policy'] = wphs_get_permissions_policy_header();
+
+    return $headers;
+}
+
+function wphs_get_csp_header(): string {
+    $csp = get_option('wphs_Content-Security-Policy_Custom');
+    return empty($csp) ? WPHS_STANDARD_VALUE_CSP : $csp;
+}
+
+function wphs_get_permissions_policy_header(): string {
+    $pp = get_option('wphs_Permissions-Policy');
+    return empty($pp) ? WPHS_STANDARD_VALUE_PERMISSIONS_POLICY : $pp;
+}
+
+add_filter('wp_headers', 'wphs_get_headers');
+
+function wphs_settings_page() {
     ?>
     <div class="wrap">
         <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
         <p style="font-size: 20px; font-weight: bold; text-align: center;"><?php _e('Configure the security headers for your website below:', 'WPHeaderSecurity'); ?></p>
         <button id="checkSecurityHeadersButton" style="cursor: pointer; padding: 10px 20px; background: black; color: #fff; font-size: 18px; border-radius: 20px; margin: auto; display: block;"><?php _e('Check Website Security Headers', 'WPHeaderSecurity'); ?></button>
         <form method="post" action="options.php">
-            <?php settings_fields('wp_header_security'); ?>
-            <?php do_settings_sections('wp_header_security'); ?>
+            <?php settings_fields('wphs_settings'); ?>
+            <?php do_settings_sections('wphs_settings'); ?>
             <?php submit_button(); ?>
         </form>
     </div>
@@ -63,70 +69,64 @@ function wp_header_security_settings_page() {
     </script>
     <?php
 }
-function wp_header_security_settings_init() {
-    register_setting('wp_header_security', 'wp_header_security_X-Frame-Options', 'sanitize_text_field');
-    register_setting('wp_header_security', 'wp_header_security_Strict-Transport-Security', 'sanitize_text_field');
-    register_setting('wp_header_security', 'wp_header_security_Content-Security-Policy_Select', 'sanitize_csp');
-    register_setting('wp_header_security', 'wp_header_security_Content-Security-Policy_Custom', 'sanitize_csp');
-    register_setting('wp_header_security', 'wp_header_security_X-Content-Type-Options', 'sanitize_text_field');
-    register_setting('wp_header_security', 'wp_header_security_Referrer-Policy', 'sanitize_text_field');
-    register_setting('wp_header_security', 'wp_header_security_Permissions-Policy', 'sanitize_text_field');
-    add_settings_section('wp_header_security_section', 'Security Headers', function() {}, 'wp_header_security');
 
+function wphs_settings_init() {
+    register_setting('wphs_settings', 'wphs_X-Frame-Options', 'sanitize_text_field');
+    register_setting('wphs_settings', 'wphs_Strict-Transport-Security', 'sanitize_text_field');
+    register_setting('wphs_settings', 'wphs_Content-Security-Policy_Custom', 'sanitize_csp');
+    register_setting('wphs_settings', 'wphs_X-Content-Type-Options', 'sanitize_text_field');
+    register_setting('wphs_settings', 'wphs_Referrer-Policy', 'sanitize_text_field');
+    register_setting('wphs_settings', 'wphs_Permissions-Policy', 'sanitize_text_field');
 
-    add_settings_field('wp_header_security_X-Frame-Options', 'X-Frame-Options', function() {
-        $value = get_option('wp_header_security_X-Frame-Options', 'SAMEORIGIN');
+    add_settings_section('wphs_section', 'Security Headers', function() {}, 'wphs_settings');
+
+    add_settings_field('wphs_X-Frame-Options', 'X-Frame-Options', function() {
+        $value = get_option('wphs_X-Frame-Options', 'SAMEORIGIN');
         $choices = array(
             'SAMEORIGIN' => 'SAMEORIGIN (recommended)',
             'ALLOW-FROM' => 'ALLOW-FROM',
             'DENY' => 'DENY'
         );
-        echo '<select name="wp_header_security_X-Frame-Options">';
+        echo '<select name="wphs_X-Frame-Options">';
         foreach ($choices as $key => $label) {
             $selected = ($value == $key) ? 'selected' : '';
-            echo '<option value="' . esc_attr( $key ) . '" ' . esc_attr( $selected ) . '>' . esc_html( $label ) . '</option>';
+            echo '<option value="' . esc_attr($key) . '" ' . esc_attr($selected) . '>' . esc_html($label) . '</option>';
         }
         echo '</select>';
-        echo '<p class="description">The X-Frame-Options header restricts which websites can embed your site\'s content in an iframe. 
-                <strong>SAMEORIGIN</strong> allows only the same domain to embed content. 
-                <strong>ALLOW-FROM</strong> allows a specific domain to embed content. 
-                <strong>DENY</strong> blocks all domains from embedding content.</p>';
-    }, 'wp_header_security', 'wp_header_security_section');
+    }, 'wphs_settings', 'wphs_section');
 
-    add_settings_field('wp_header_security_Strict-Transport-Security', 'Strict-Transport-Security', function() {
-        $value = get_option('wp_header_security_Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+    add_settings_field('wphs_Strict-Transport-Security', 'Strict-Transport-Security', function() {
+        $value = get_option('wphs_Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
         $choices = array(
             'max-age=31536000; includeSubDomains' => 'max-age=31536000; includeSubDomains (good starting point)',
             'max-age=31536000; includeSubDomains; preload' => 'max-age=31536000; includeSubDomains; preload',
             'max-age=86400; includeSubDomains' => 'max-age=86400; includeSubDomains',
             'max-age=86400; includeSubDomains; preload' => 'max-age=86400; includeSubDomains; preload'
         );
-        echo '<select name="wp_header_security_Strict-Transport-Security">';
+        echo '<select name="wphs_Strict-Transport-Security">';
         foreach ($choices as $key => $label) {
             $selected = ($value == $key) ? 'selected' : '';
-            echo '<option value="' . esc_attr( $key ) . '" ' . esc_attr( $selected ) . '>' . esc_html( $label ) . '</option>';
+            echo '<option value="' . esc_attr($key) . '" ' . esc_attr($selected) . '>' . esc_html($label) . '</option>';
         }
         echo '</select>';
-        echo '<p class="description"> Strict-Transport-Security is an HTTP response header that instructs the browser to only use HTTPS to access the website. This can help protect your website from certain types of attacks, such as man-in-the-middle attacks. The value of this header should include the maximum age, in seconds, that the browser should remember to only use HTTPS.</p>';
-    }, 'wp_header_security', 'wp_header_security_section');
+    }, 'wphs_settings', 'wphs_section');
 
-    add_settings_field('wp_header_security_X-Content-Type-Options', 'X-Content-Type-Options', function() {
-        $value = get_option('wp_header_security_X-Content-Type-Options', 'nosniff');
+    add_settings_field('wphs_X-Content-Type-Options', 'X-Content-Type-Options', function() {
+        $value = get_option('wphs_X-Content-Type-Options', 'nosniff');
         $choices = array(
             'nosniff' => 'nosniff (recommended)',
             'none' => 'none'
         );
-        echo '<select name="wp_header_security_X-Content-Type-Options">';
+        echo '<select name="wphs_X-Content-Type-Options">';
         foreach ($choices as $key => $label) {
             $selected = ($value == $key) ? 'selected' : '';
-            echo '<option value="' . esc_attr( $key ) . '" ' . esc_attr( $selected ) . '>' . esc_html( $label ) . '</option>';
+            echo '<option value="' . esc_attr($key) . '" ' . esc_attr($selected) . '>' . esc_html($label) . '</option>';
         }
         echo '</select>';
-        echo '<p class="description">The X-Content-Type-Options header is a security feature that helps to prevent content sniffing, which is the process of guessing the content type of a file based on its contents. This feature is particularly useful for preventing certain types of attacks, such as cross-site scripting (XSS) and code injection, that can occur when a server sends a file with an unexpected content type. By setting the X-Content-Type-Options header to "nosniff", the browser is instructed to always use the declared content type of the file and not to guess or override it. This can help to ensure that the browser doesn\'t execute malicious content disguised as a different file type.</p>';
-    }, 'wp_header_security', 'wp_header_security_section');
+    }, 'wphs_settings', 'wphs_section');
 
-    add_settings_field('wp_header_security_Referrer-Policy', 'Referrer-Policy', function() {
-        $value = get_option('wp_header_security_Referrer-Policy', 'strict-origin-when-cross-origin');
+    add_settings_field('wphs_Referrer-Policy', 'Referrer-Policy', function() {
+        $value = get_option('wphs_Referrer-Policy', 'strict-origin-when-cross-origin');
         $choices = array(
             'no-referrer' => 'no-referrer',
             'no-referrer-when-downgrade' => 'no-referrer-when-downgrade',
@@ -137,75 +137,68 @@ function wp_header_security_settings_init() {
             'strict-origin-when-cross-origin' => 'strict-origin-when-cross-origin',
             'unsafe-url' => 'unsafe-url'
         );
-        echo '<select name="wp_header_security_Referrer-Policy">';
-        foreach ($choices as $key => $label) {
-            $selected = ($value == $key) ? 'selected' : '';
-            echo '<option value="' . esc_attr( $key ) . '" ' . esc_attr( $selected ) . '>' . esc_html( $label ) . '</option>';
-        }
-        echo '</select>';
-        echo '<p class="description">It\'s recommended to use \'strict-origin-when-cross-origin\' or \'same-origin\' for most websites, but the best choice depends on the specific security requirements and functionality of the website.</p>';
-    }, 'wp_header_security', 'wp_header_security_section');
-
-    add_settings_field('wp_header_security_Content-Security-Policy_Select', 'Content-Security-Policy Dropdown', function() {
-        $value = get_option('wp_header_security_Content-Security-Policy_Select', "default-src 'self'; script-src 'self' 'unsafe-inline'; img-src 'self' data:;");
-        $choices = array(
-            "default-src 'self'; script-src 'self' 'unsafe-inline'; img-src 'self' data:;" => "default-src 'self'; script-src 'self' 'unsafe-inline'; img-src 'self' data:;",
-            "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self';" => "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self';",
-            "default-src 'self'; font-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;" => "default-src 'self'; font-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;"
-        );
-        echo '<select name="wp_header_security_Content-Security-Policy_Select">';
+        echo '<select name="wphs_Referrer-Policy">';
         foreach ($choices as $key => $label) {
             $selected = ($value == $key) ? 'selected' : '';
             echo '<option value="' . esc_attr($key) . '" ' . esc_attr($selected) . '>' . esc_html($label) . '</option>';
         }
         echo '</select>';
-        echo '<p class="description">Choose a predefined Content-Security-Policy or specify your own below.</p>';
-    }, 'wp_header_security', 'wp_header_security_section');
+    }, 'wphs_settings', 'wphs_section');
 
-    add_settings_field('wp_header_security_Content-Security-Policy_Custom', 'Custom Content-Security-Policy', function() {
-        $value = get_option('wp_header_security_Content-Security-Policy_Custom', "");
-        echo '<label for="wp_header_security_Content-Security-Policy_Custom">Specify Custom Policy: </label>';
-        echo '<input type="text" id="wp_header_security_Content-Security-Policy_Custom" name="wp_header_security_Content-Security-Policy_Custom" value="' . esc_attr($value) . '" style="width: 100%;" />';
-        echo '<p class="description">Note: If you choose a Custom CSP, other CSP options will be invalid. Please ensure you understand the syntax and implications of your custom policy. Common custom directives include: 
-                <br>- <strong>script-src</strong>: Defines which scripts can run.
-                <br>- <strong>img-src</strong>: Defines which images can be loaded.
-                <br>- <strong>style-src</strong>: Defines which stylesheets can be applied.
-                <br>... and many more.
-                <br><br><strong>Warning: Incorrect configuration of the Content-Security-Policy can lead to site breakage or potential security vulnerabilities. Proceed with caution<strong></p>';
-    }, 'wp_header_security', 'wp_header_security_section');
+    add_settings_field('wphs_Content-Security-Policy_Custom', 'Custom Content-Security-Policy', function() {
+        $value = get_option('wphs_Content-Security-Policy_Custom', "");
+        echo '<label for="wphs_Content-Security-Policy_Custom">Specify Custom Policy: </label>';
+        echo '<input type="text" id="wphs_Content-Security-Policy_Custom" name="wphs_Content-Security-Policy_Custom" value="' . esc_attr($value) . '" style="width: 100%;" />';
+        echo '<p class="description">Note: If you choose a Custom CSP, other CSP options will be invalid. Please ensure you understand the syntax and implications of your custom policy.</p>';
+    }, 'wphs_settings', 'wphs_section');
 
-    add_settings_field('wp_header_security_Permissions-Policy', 'Permissions-Policy', function() {
-        $value = get_option('wp_header_security_Permissions-Policy', "geolocation 'none'; camera 'none'; microphone 'none';");
-
-        // Dropdown selection
-        $choices = array(
-            "geolocation 'none'; camera 'none'; microphone 'none';" => "geolocation 'none'; camera 'none'; microphone 'none'; (recommended)",
-            "geolocation 'self'; camera 'self'; microphone 'self';" => "geolocation 'self'; camera 'self'; microphone 'self';",
-            "geolocation 'none'; camera 'self'; microphone 'self';" => "geolocation 'none'; camera 'self'; microphone 'self';",
-            "geolocation 'self'; camera 'none'; microphone 'none';" => "geolocation 'self'; camera 'none'; microphone 'none';"
-        );
-        echo '<select name="wp_header_security_Permissions-Policy_Select">';
-        foreach ($choices as $key => $label) {
-            $selected = ($value == $key) ? 'selected' : '';
-            echo '<option value="' . esc_attr( $key ) . '" ' . esc_attr( $selected ) . '>' . esc_html( $label ) . '</option>';
-        }
-        echo '</select>';
-    }, 'wp_header_security', 'wp_header_security_section');
+    add_settings_field('wphs_Permissions-Policy', 'Permissions-Policy', function() {
+        $value = get_option('wphs_Permissions-Policy', WPHS_STANDARD_VALUE_PERMISSIONS_POLICY);
+        echo '<textarea id="wphs_Permissions-Policy" name="wphs_Permissions-Policy" rows="5" cols="50" style="width: 100%;">' . esc_textarea($value) . '</textarea>';
+    }, 'wphs_settings', 'wphs_section');
 }
 
-add_action('admin_init', 'wp_header_security_settings_init');
+add_action('admin_init', 'wphs_settings_init');
 add_action('admin_menu', function() {
-    add_options_page('WP Header Security', 'WP Header Security', 'manage_options', 'wp_header_security', 'wp_header_security_settings_page');
+    add_options_page('WP Header Security', 'WP Header Security', 'manage_options', 'wphs_settings', 'wphs_settings_page');
 });
 
 function sanitize_csp($value) {
     // Remove any HTML tags
     $value = wp_strip_all_tags($value);
-
     // Removing any special characters that don't belong in CSPs
     $value = preg_replace('/[^a-zA-Z0-9-;:\.\/\* \'"]/', '', $value);
-
-    // Return sanitized value
     return $value;
 }
 
+function wphs_activate() {
+    if (!get_option('wphs_X-Frame-Options')) {
+        add_option('wphs_X-Frame-Options', 'SAMEORIGIN');
+    }
+    if (!get_option('wphs_Strict-Transport-Security')) {
+        add_option('wphs_Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+    }
+    if (!get_option('wphs_Content-Security-Policy_Custom')) {
+        add_option('wphs_Content-Security-Policy_Custom', WPHS_STANDARD_VALUE_CSP);
+    }
+    if (!get_option('wphs_X-Content-Type-Options')) {
+        add_option('wphs_X-Content-Type-Options', 'nosniff');
+    }
+    if (!get_option('wphs_Referrer-Policy')) {
+        add_option('wphs_Referrer-Policy', 'strict-origin-when-cross-origin');
+    }
+    if (!get_option('wphs_Permissions-Policy')) {
+        add_option('wphs_Permissions-Policy', WPHS_STANDARD_VALUE_PERMISSIONS_POLICY);
+    }
+}
+register_activation_hook(__FILE__, 'wphs_activate');
+
+function wphs_deactivate() {
+    delete_option('wphs_X-Frame-Options');
+    delete_option('wphs_Strict-Transport-Security');
+    delete_option('wphs_Content-Security-Policy_Custom');
+    delete_option('wphs_X-Content-Type-Options');
+    delete_option('wphs_Referrer-Policy');
+    delete_option('wphs_Permissions-Policy');
+}
+register_deactivation_hook(__FILE__, 'wphs_deactivate');
